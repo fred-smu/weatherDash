0bd79344fa9c4073f3c800916419b7555b9820ab
Fred Guthrie <fred3@myself.com>
2020-02-06 00:01:37 -0600

    everything good

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ var APIKey = "1caea8931f6bdb95be52d99e2752e33e";
 var cityCount = 0;
 var citySave = "";
 var count = 1;
+var utc = new Date().toJSON().slice(0,10);
+var myyear = utc.slice(0,4);
+var mymonth = utc.slice(5,7);
+var myday = parseInt(utc.slice(8,10));
+var mydate=" ";
+dateCall(0);
 
+//pulls from local storage and sets the history(needs work)
 for (let index = 1; index < 5; index++) {
   $('#old-search').empty();
   count++;
@@ -14,48 +21,33 @@ for (let index = 1; index < 5; index++) {
     citySave = localStorage.getItem("cityList" + index);
     var newListItem = '<li class = "cityhist">'+ cityIn+ '</li>';
     $('#old-search').append(newListItem);
-    console.log(".append "+newListItem);
-    // var newListItem = '<li class = "cityhist">'+ oldCity+ '</li>';
-      // console.log("newListItem "+newListItem);
-      // $('#old-search').append(newListItem);
-   
    }
-    // console.log(oldCity);
- 
-  
 }
 
+// starts the search once the search button is clicked
 $("#getWeather").on("click", function () {
- 
   cityIn = $("#city-search").val();
-  citySave = $("#city-search").val();
-  // localStorage.setItem('cityList'+ count, cityIn);
+   localStorage.setItem('city', cityIn);
    var newListItem = '<li class = "cityhist">'+ cityIn+ '</li>';
    $('#old-search').append(newListItem);
    apiCall();
    
 });
 
-$("#old-search li").click(function () {
-  console.log("on click");
-  // drawHistory();
-  cityIn = $(this).val();
-  citySave = $(this).val();
-  console.log("on click " + citySave);
+//click event on the li's in histroy
+$(document).on("click", ".cityhist", function() {
+  cityIn=$(event.target).text();
+  $("#city-search").attr("placeholder","");
+  $("#city-search").attr("placeholder", $(event.target).text());
   apiCall();
-});  
+})
+  
 
 function apiCall() {
   var APIKey = "166a433c57516f51dfab1f7edaed8413";
-
-
-  https://samples.openweathermap.org/data/2.5/forecast/daily?id=524901&appid=b1b15e88fa797225412429c1c50c122a1
-  // Here we are building the URL we need to query the database
   var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityIn +
     "&appid=" + APIKey + "&units=imperial"
-
-
-  // var queryURL = "api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7&appid=" + APIKey;
+ // var queryURL = "api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7&appid=" + APIKey;
   // Here we run our AJAX call to the OpenWeatherMap API
   $.ajax({
     url: queryURL,
@@ -63,45 +55,90 @@ function apiCall() {
   })
     // We store all of the retrieved data inside of an object called "response"
     .then(function (response) {
-
-      // Log the queryURL
-      console.log(queryURL);
-
-      // Log the resulting object
-      console.log(response);
-
+      var lat =" ";
+      var lon =" ";
+     // loops thu the arrys to pull and set data and display values.
       var index = 0;
       for (let i = 0; i < 7; i++) {
+        dateCall(i);
         let cur = response.list[i];
-        $("#cityName" + i).text(response.city.name);
-        // console.log("#cityName" + i +"    name "+ response.city.name);
-        // console.log("#cityName" + i +"    icon "+ cur.weather[i].icon);
+        var utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+        myyear = utc.slice(0,4);
+        mymonth = utc.slice(5,2);
+        myday = utc.slice(8,2);
+        $("#cityName" + i).text(response.city.name +" "+mydate );
+      
         $("#curWeathIcn"+ i).attr("src", `http://openweathermap.org/img/w/${cur.weather[0].icon}.png`);
-        //  // Converts the temp to Kelvin with the below formula
-        // var tempF = ((response.main.main[0]));
-        // temp - 273.15) * 1.80 + 32);
         $("#curTemp" + i).text("Temp: " + cur.main.temp);
         $("#curHum" + i).text("Humidity: " + cur.main.humidity + "%");
         $("#curWind" + i).text("Windspeed: " + cur.wind.speed + " MPH");
-        // cur.weather[0].description,
+        lat = response.city.coord.lat;
+        lon = response.city.coord.lon;
       };
+      // call to teh UV api to set the UV values and colors
+      uvApiCall(lat,lon);
     });
-  // newCityHist();
+};
+
+function uvApiCall(lat,lon) {
+  var APIKey = "166a433c57516f51dfab1f7edaed8413";
+  var queryURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=37.75&lon=-122.37&appid=" + APIKey;
+ // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+     var index = 0;
+      //loops thu the arry to pull data
+      for (let i = 0; i < 7; i++) {
+       
+        let cur = response[i];
+        let uvF = parseInt(cur.value);
+        //sets the value        
+        $("#curUv" + i).text("UV Index: "+(cur.value));
+        //add/deletes the class so the colors are set acording to UV rating
+        if (uvF < 3) { 
+          $("#curUv" + i).removeClass('violet');
+          $("#curUv" + i).addClass('green');
+        } else if (uvF < 6) { 
+          $("#curUv" + i).removeClass('violet');
+          $("#curUv" + i).addClass('yellow');
+        } else if (uvF < 8) { 
+          $("#curUv" + i).removeClass('violet');
+          $("#curUv" + i).addClass('orange');
+        } else if (uvF < 11) { 
+          $("#curUv" + i).removeClass('violet');
+          $("#curUv" + i).addClass('red');
+        }
+     };
+    });
 };
 
 function drawHistory() {
   // function to pull city history from local memory
  
   let cityList = localStorage.getItem("cityList");
-console.log("cityList =" +cityList)
   $('#old-search').empty();
   $('<div>')
     let cityNameDiv = $('<div>');
     cityNameDiv.addClass("cityList");
     cityNameDiv.attr("value",cityList);
     cityNameDiv.text(cityList);
-    console.log("cityNameDiv =" +cityNameDiv)
     $('#old-search').append(cityNameDiv);
-  // }
-  
 };
+
+//my custom date funtion
+function dateCall(itemNum) {
+myyear = utc.slice(0,4);
+mymonth = utc.slice(5,7);
+myday = parseInt(utc.slice(8,10));
+myday = myday + itemNum;
+if (myday.length = 1 ){
+  mydate=mymonth+"/0"+myday+"/"+myyear;
+}else{
+  mydate=mymonth+"/"+myday+"/"+myyear;
+}
+  
+}
